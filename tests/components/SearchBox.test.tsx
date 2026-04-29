a66b2e3e456f15dce004ca2000a3d1149b797790
Yohannes Getachew <demisgech@gmail.com>
2026-04-29 22:52:38 +0300

    Testing user-interactions

---
+++ tests/components/SearchBox.test.tsx
@@ -0,0 +1,40 @@
+import { userEvent } from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { it, expect, describe, vi } from "vitest";
+import SearchBox from "../../src/components/SearchBox";
+
+describe("SearchBox", () => {
+  const renderSearchBox = () => {
+    const onChange = vi.fn();
+    render(<SearchBox onChange={onChange} />);
+
+    return {
+      input: screen.getByPlaceholderText(/search/i),
+      user: userEvent.setup(),
+      onChange,
+    };
+  };
+
+  it("should render an input field for searching", () => {
+    const { input } = renderSearchBox();
+
+    expect(input).toBeInTheDocument();
+  });
+
+  it("should call onChange when Enter is pressed", async () => {
+    const { input, onChange, user } = renderSearchBox();
+
+    const searchTerm = "SearchTerm";
+    await user.type(input, searchTerm + "{enter}");
+
+    expect(onChange).toHaveBeenCalledWith(searchTerm);
+  });
+
+  it("should not call onChange when input field is empty", async () => {
+    const { input, onChange, user } = renderSearchBox();
+
+    await user.type(input, "{enter}");
+
+    expect(onChange).not.toHaveBeenCalled();
+  });
+});
--- tests/components/TermsAndContions.test.tsx
+++ tests/components/TermsAndContions.test.tsx
@@ -4,37 +4,29 @@ import { it, expect, describe } from "vitest";
 import TermsAndConditions from "../../src/components/TermsAndConditions";
 
 describe("TermsAndConditions", () => {
-  it("should render Terms and Conditions with its initial state", () => {
+  const renderComponent = () => {
     render(<TermsAndConditions />);
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button"),
+    };
+  };
 
-    const heading = screen.getByRole("heading");
+  it("should render Terms and Conditions with its initial state", () => {
+    const { heading, checkbox, button } = renderComponent();
 
-    expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent("Terms & Conditions");
-
-    const paragraph = screen.getByRole("paragraph");
-
-    expect(paragraph).toBeInTheDocument();
-    expect(paragraph).toHaveTextContent(/lorem/i);
-
-    const checkbox = screen.getByRole("checkbox");
-
-    expect(checkbox).toBeInTheDocument();
     expect(checkbox).not.toBeChecked();
-
-    const button = screen.getByRole("button");
-
-    expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
   });
 
   it("should enable the button when the checkbox is checked", async () => {
-    render(<TermsAndConditions />);
+    const { checkbox, button } = renderComponent();
 
-    const checkbox = screen.getByRole("checkbox");
     const user = userEvent.setup();
     await user.click(checkbox);
 
-    expect(screen.getByRole("button")).toBeEnabled();
+    expect(button).toBeEnabled();
   });
 });
